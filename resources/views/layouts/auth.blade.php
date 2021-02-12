f69dcd0565d59c338a071554fefdcdfab2463692
Oladotun Denton <dotundenton@gmail.com>
2021-02-12 14:56:52 +0100

    Added font awesome cdn

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -11,6 +11,7 @@
 
     <link href="{{ secure_asset('assets/css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ secure_asset('assets/font-awesome/css/font-awesome.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
 
     <link href="{{ secure_asset('assets/css/animate.css') }}" rel="stylesheet">
     <link href="{{ secure_asset('assets/css/style.css') }}" rel="stylesheet">

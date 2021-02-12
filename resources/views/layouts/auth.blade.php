4f807dd71761608fd04f27bbe49ac8fb65f5f9a8
Oladotun Denton <dotundenton@gmail.com>
2021-02-12 11:17:15 +0100

    Added the secure_asset for https rendering of assets files

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -9,11 +9,11 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">    
     <title>{{ config('app.name', 'NGR') }}</title>
 
-    <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/font-awesome/css/font-awesome.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('assets/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('assets/font-awesome/css/font-awesome.css') }}" rel="stylesheet">
 
-    <link href="{{ asset('assets/css/animate.css') }}" rel="stylesheet">
-    <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('assets/css/animate.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('assets/css/style.css') }}" rel="stylesheet">
 
 </head>
 
